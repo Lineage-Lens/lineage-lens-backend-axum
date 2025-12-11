5145cbf76e74497ac2ff41cd6127b7a0b6c3da0b
Tom <tom.weelborg@gmail.com>
2025-12-11 17:24:41 +0100

    feat: add relationship repositories to AppState

--- src/state.rs
+++ src/state.rs
@@ -1,9 +1,13 @@
 use crate::crud::person::PersonRepository;
 use sqlx::{MySql, Pool};
 use std::sync::Arc;
+use crate::crud::person_relationship_link::PersonRelationshipLinkRepository;
+use crate::crud::relationship::RelationshipRepository;
 
 pub struct AppState {
     pub person_repository: PersonRepository,
+    pub person_relationship_link_repository: PersonRelationshipLinkRepository,
+    pub relationship_repository: RelationshipRepository,
     pub google_oauth2_client_id: String,
 }
 
@@ -13,7 +17,13 @@ impl AppState {
 
         return AppState {
             person_repository: PersonRepository {
-                pool
+                pool: Arc::clone(&pool)
+            },
+            person_relationship_link_repository: PersonRelationshipLinkRepository {
+                pool: Arc::clone(&pool)
+            },
+            relationship_repository: RelationshipRepository {
+                pool: Arc::clone(&pool)
             },
             google_oauth2_client_id,
         };
