cd7a77d521e493bcc073af12690f0f4558bfd116
Tom <tom.weelborg@gmail.com>
2025-12-11 10:29:05 +0100

    feat: add db support

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,16 @@ mod api;
 
 use std::net::{IpAddr, Ipv4Addr};
 use dotenvy::dotenv;
+use sqlx::mysql::MySqlPoolOptions;
 
 #[tokio::main]
 async fn main() {
     dotenv().ok();
 
     let port = std::env::var("PORT").unwrap().parse::<u16>().unwrap();
+
+    let url = std::env::var("DATABASE_URL").unwrap();
+    let pool = MySqlPoolOptions::new().max_connections(10).connect(&url).await.unwrap();
 
     api::start_server(IpAddr::V4(Ipv4Addr::UNSPECIFIED), port).await;
 }
